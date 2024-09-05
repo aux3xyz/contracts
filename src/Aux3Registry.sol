f1b6ab8a506b2ab13c7a9abc6f30619aac14c75e
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-09-05 19:33:46 +0530

    updates to topic handling, eventAction

--- src/Aux3Registry.sol
+++ src/Aux3Registry.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 struct Aux3EventConfig {
-    address contractAddress;
-    bytes32 eventSignature;
-    bytes32 eventAction;
-    uint256 aux3Id;
     uint32 chainId;
-    uint8 eventTopic;
+    uint256 aux3Id;
+    address contractAddress;
+    bytes32 topic_0;
+    bytes32 topic_1;
+    bytes32 topic_2;
+    bytes32 topic_3;
 }
 
 contract Aux3Registry is Ownable {
@@ -17,6 +18,7 @@ contract Aux3Registry is Ownable {
     uint256 public lastAux3EventId;
     mapping(address => uint256) public aux3Ids;
     mapping(uint256 => Aux3EventConfig) public aux3EventIds;
+    mapping(uint256 => bytes) public aux3EventActions;
 
     // initialize the contract
     constructor(address _owner) Ownable(_owner) {}
@@ -67,49 +69,44 @@ contract Aux3Registry is Ownable {
 
     // @dev register an event for user having an aux3 id
     function registerAux3Event(
-        address _contractAddress,
-        bytes32 _eventSignature,
-        bytes32 _eventAction,
         uint32 _chainId,
-        uint8 _eventTopic
+        address _contractAddress,
+        bytes32 _topic_0,
+        bytes32 _topic_1,
+        bytes32 _topic_2,
+        bytes32 _topic_3,
+        bytes memory _eventAction
     ) public {
-        require(aux3Ids[msg.sender] != 0, "Sender does not have an ID");
+        uint256 _aux3Id = aux3Ids[msg.sender];
+
+        require(_aux3Id != 0, "Sender does not have an ID");
         require(_contractAddress != address(0), "Invalid contract address");
-        require(_eventSignature != bytes32(0), "Invalid event signature");
-        require(_eventAction != bytes32(0), "Invalid event action");
+        require(_topic_0 != bytes32(0), "Invalid event signature");
+        require(_eventAction.length > 0, "Invalid event action");
         require(_chainId != 0, "Invalid chain ID");
-        require(_eventTopic != 0, "Invalid event topic");
 
         lastAux3EventId++;
 
         Aux3EventConfig memory config =
-            Aux3EventConfig(_contractAddress, _eventSignature, _eventAction, aux3Ids[msg.sender], _chainId, _eventTopic);
+            Aux3EventConfig(_chainId, _aux3Id, _contractAddress, _topic_0, _topic_1, _topic_2, _topic_3);
 
         aux3EventIds[lastAux3EventId] = config;
+        aux3EventActions[lastAux3EventId] = _eventAction;
 
         // Emit an event for registration
-        emit Aux3EventRegistered(_contractAddress, lastAux3EventId, aux3Ids[msg.sender]);
+        emit Aux3EventRegistered(_contractAddress, lastAux3EventId, _aux3Id);
     }
 
     // @dev get the aux3 event config for an aux3Event id
-    function getAux3EventConfig(uint256 _eventId) public view returns (Aux3EventConfig memory) {
-        return aux3EventIds[_eventId];
+    function getAux3Event(uint256 _eventId) public view returns (Aux3EventConfig memory, bytes memory) {
+        return (aux3EventIds[_eventId], aux3EventActions[_eventId]);
     }
 
     // @dev update the aux3 event action for an _eventId
-    function updateAux3EventConfig(uint256 _eventId, bytes32 _eventAction) public {
+    function updateAux3EventAction(uint256 _eventId, bytes memory _eventAction) public {
         require(aux3EventIds[_eventId].aux3Id == aux3Ids[msg.sender], "Sender does not own the event's aux3Id");
 
-        Aux3EventConfig memory config = Aux3EventConfig(
-            aux3EventIds[_eventId].contractAddress,
-            aux3EventIds[_eventId].eventSignature,
-            _eventAction,
-            aux3EventIds[_eventId].aux3Id,
-            aux3EventIds[_eventId].chainId,
-            aux3EventIds[_eventId].eventTopic
-        );
-
-        aux3EventIds[_eventId] = config;
+        aux3EventActions[_eventId] = _eventAction;
 
         emit Aux3EventUpdated(_eventId);
     }
