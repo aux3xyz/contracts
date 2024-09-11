3e990c7a2efdc305f8ab334a6fbb473f2066fbe5
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-09-11 11:13:21 +0530

    added layerZero send to payments contract

--- src/Aux3Payments.sol
+++ src/Aux3Payments.sol
@@ -3,9 +3,31 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OApp, MessagingFee, Origin} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 
-contract Aux3Payments is Ownable {
-    constructor(address initialOwner) Ownable(initialOwner) {}
+abstract contract Aux3Payments is Ownable, OApp {
+    uint32 dstId;
+    address lzEndpoint;
+
+    using OptionsBuilder for bytes;
+
+    constructor(address _initialOwner, address _endpoint, uint32 _dstId)
+        Ownable(_initialOwner)
+        OApp(_endpoint, _initialOwner)
+    {
+        dstId = _dstId;
+        lzEndpoint = _endpoint;
+    }
+
+    function changeLzDelegate() external onlyOwner {
+        setDelegate(msg.sender);
+    }
+
+    function _send(bytes memory _message) internal {
+        bytes memory _options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200000, 10);
+        _lzSend(dstId, _message, _options, MessagingFee(msg.value, 0), payable(msg.sender));
+    }
 
     // emits when payment is received with native token
     event PaymentReceived(uint256 indexed aux3Id, address sender, uint256 amount);
@@ -18,15 +40,20 @@ contract Aux3Payments is Ownable {
         require(msg.value > 0, "Must provide a non-zero amount");
         require(aux3Id > 0, "Must provide a valid aux3 id");
         payable(owner()).transfer(msg.value);
+
+        _send(abi.encode("PaymentReceived", aux3Id, msg.sender, msg.value));
+
         emit PaymentReceived(aux3Id, msg.sender, msg.value);
     }
 
     // @dev takes payment with ERC20 token and forwards to owner
-    function payWithERC20(uint256 aux3Id, address token, uint256 amount) external {
+    function payWithERC20(uint256 aux3Id, address token, uint256 amount) external payable {
         require(amount > 0, "Must provide a non-zero amount");
         require(aux3Id > 0, "Must provide a valid aux3 id");
         require(token != address(0), "Must provide a valid token address");
         IERC20(token).transferFrom(msg.sender, owner(), amount);
+
+        _send(abi.encode("PaymentReceivedWithERC20", aux3Id, msg.sender, token, amount));
 
         emit PaymentReceivedWithERC20(aux3Id, msg.sender, token, amount);
     }
