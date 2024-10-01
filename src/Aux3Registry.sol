a2f0e967b68633ca2cb810c519ce223b9f0145f3
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-10-01 14:17:18 +0530

    revamp the contract and comments

--- src/Aux3Registry.sol
+++ src/Aux3Registry.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/// @notice Configuration struct for Aux3Events
 struct Aux3EventConfig {
     uint32 chainId;
     uint256 aux3Id;
@@ -14,49 +16,57 @@ struct Aux3EventConfig {
     bytes32 topic_3;
 }
 
+/// @title Aux3Registry
 contract Aux3Registry is Ownable {
     uint256 public lastId;
     uint256 public lastAux3EventId;
+
+    using SafeERC20 for IERC20;
+
+    /// @notice Mapping of addresses to their Aux3Ids
     mapping(address => uint256) public aux3Ids;
+    /// @notice Mapping of Aux3Event IDs to their configurations
     mapping(uint256 => Aux3EventConfig) public aux3EventIds;
+    /// @notice Mapping of Aux3Event IDs to their actions
     mapping(uint256 => bytes) public aux3EventActions;
 
-    // initialize the contract
-    constructor(address _owner) Ownable(_owner) {}
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
-    // event declarations
-    // aux3 ID related
+    // Events
     event Aux3IdRegistered(address indexed addr, uint256 id);
     event Aux3IdTransferred(address indexed from, address indexed to, uint256 id);
-
-    // aux3 events related
     event Aux3EventRegistered(address indexed contractAddress, uint256 indexed eventId, uint256 indexed aux3Id);
     event Aux3EventUpdated(uint256 indexed eventId);
+    event Aux3IdDeleted(address indexed addr, uint256 id);
+    event Aux3EventDeleted(uint256 indexed eventId);
+    event NativeTokenSwept(address indexed owner, uint256 amount);
+    event ERC20TokenSwept(address indexed token, address indexed owner, uint256 amount);
 
-    // @dev register an address for an aux3 id
-    function registerAux3Id(address _addr) public returns (uint256) {
-        require(aux3Ids[_addr] == 0, "Address is already registered");
-        require(_addr != address(0), "Invalid address");
+    /// @notice Registers an Aux3Id for the sender
+    /// @return The newly registered Aux3Id
+    function registerAux3Id() public returns (uint256) {
+        require(aux3Ids[msg.sender] == 0, "Address is already registered");
 
         lastId++;
-        aux3Ids[_addr] = lastId;
+        aux3Ids[msg.sender] = lastId;
 
-        // Emit an event for registration
-        emit Aux3IdRegistered(_addr, lastId);
-
+        emit Aux3IdRegistered(msg.sender, lastId);
         return lastId;
     }
 
-    // @dev get the aux3 id for an address
+    /// @notice Retrieves the Aux3Id of a given address
+    /// @param _addr The address to query
+    /// @return The Aux3Id associated with the address
     function getAux3Id(address _addr) public view returns (uint256) {
         return aux3Ids[_addr];
     }
 
-    // @dev transfer the aux3 id to another address
+    /// @notice Transfers the sender's Aux3Id to another address
+    /// @param _to The recipient address
     function transferAux3Id(address _to) public {
-        require(lastId > 0, "Registry is empty");
         require(_to != msg.sender, "Cannot transfer to the same address");
         require(_to != address(0), "Invalid recipient address");
+        require(aux3Ids[_to] == 0, "Recipient already has an ID");
 
         uint256 id = aux3Ids[msg.sender];
         require(id != 0, "Sender does not have an ID");
@@ -64,11 +74,26 @@ contract Aux3Registry is Ownable {
         delete aux3Ids[msg.sender];
         aux3Ids[_to] = id;
 
-        // Emit an event for transfer
         emit Aux3IdTransferred(msg.sender, _to, id);
     }
 
-    // @dev register an event for user having an aux3 id
+    /// @notice Deletes the sender's Aux3Id
+    function deleteAux3Id() public {
+        require(aux3Ids[msg.sender] != 0, "Sender does not own any Aux3Id");
+
+        uint256 idToDelete = aux3Ids[msg.sender];
+        delete aux3Ids[msg.sender];
+        emit Aux3IdDeleted(msg.sender, idToDelete);
+    }
+
+    /// @notice Registers a new Aux3Event
+    /// @param _chainId The chain ID for the event
+    /// @param _contractAddress The contract address for the event
+    /// @param _topic_0 The first topic (event signature)
+    /// @param _topic_1 The second topic (optional)
+    /// @param _topic_2 The third topic (optional)
+    /// @param _topic_3 The fourth topic (optional)
+    /// @param _eventAction The action associated with the event
     function registerAux3Event(
         uint32 _chainId,
         address _contractAddress,
@@ -88,36 +113,61 @@ contract Aux3Registry is Ownable {
 
         lastAux3EventId++;
 
+        // create Aux3EventConfig
         Aux3EventConfig memory config =
             Aux3EventConfig(_chainId, _aux3Id, _contractAddress, _topic_0, _topic_1, _topic_2, _topic_3);
 
         aux3EventIds[lastAux3EventId] = config;
         aux3EventActions[lastAux3EventId] = _eventAction;
 
-        // Emit an event for registration
         emit Aux3EventRegistered(_contractAddress, lastAux3EventId, _aux3Id);
     }
 
-    // @dev get the aux3 event config for an aux3Event id
+    /// @notice Retrieves the configuration and action of an Aux3Event
+    /// @param _eventId The ID of the Aux3Event
+    /// @return The Aux3EventConfig and the associated action
     function getAux3Event(uint256 _eventId) public view returns (Aux3EventConfig memory, bytes memory) {
+        require(aux3EventIds[_eventId].aux3Id != 0, "Event does not exist");
         return (aux3EventIds[_eventId], aux3EventActions[_eventId]);
     }
 
-    // @dev update the aux3 event action for an _eventId
-    function updateAux3EventAction(uint256 _eventId, bytes memory _eventAction) public {
+    /// @notice Deletes an Aux3Event
+    /// @param _eventId The ID of the Aux3Event to delete
+    function deleteAux3Event(uint256 _eventId) public {
+        require(aux3EventIds[_eventId].aux3Id != 0, "Event does not exist");
         require(aux3EventIds[_eventId].aux3Id == aux3Ids[msg.sender], "Sender does not own the event's aux3Id");
 
+        delete aux3EventIds[_eventId];
+        delete aux3EventActions[_eventId];
+        emit Aux3EventDeleted(_eventId);
+    }
+
+    /// @notice Updates the action for an Aux3Event
+    /// @param _eventId The ID of the Aux3Event to update
+    /// @param _eventAction The new action for the Aux3Event
+    function updateAux3EventAction(uint256 _eventId, bytes memory _eventAction) public {
+        require(aux3EventIds[_eventId].aux3Id == aux3Ids[msg.sender], "Sender does not own the event's aux3Id");
         aux3EventActions[_eventId] = _eventAction;
         emit Aux3EventUpdated(_eventId);
     }
 
+    /// @notice Sweeps native tokens from the contract to the owner
     function sweepNativeToken() external onlyOwner {
-        uint256 _balance = address(this).balance;
-        payable(msg.sender).transfer(_balance);
+        uint256 balance = address(this).balance;
+        address payable ownerPayable = payable(owner());
+        (bool success,) = ownerPayable.call{value: balance}("");
+        require(success, "Native token transfer failed");
+
+        emit NativeTokenSwept(ownerPayable, balance);
     }
 
-    function sweepToken(address _token) external onlyOwner {
-        uint256 balance = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(msg.sender, balance);
+    /// @notice Sweeps ERC20 tokens from the contract to the owner
+    /// @param token The IERC20 token to sweep
+    function sweepToken(IERC20 token) external onlyOwner {
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "No tokens to sweep");
+        token.safeTransfer(owner(), balance);
+
+        emit ERC20TokenSwept(address(token), owner(), balance);
     }
 }
--- test/Aux3Registry.t.sol
+++ test/Aux3Registry.t.sol
@@ -6,77 +6,202 @@ import "../src/Aux3Registry.sol";
 
 contract Aux3RegistryTest is Test {
     Aux3Registry public registry;
+    address public owner;
+    address public user1;
+    address public user2;
 
-    address public user1 = address(0x1);
-    address public user2 = address(0x2);
-    address public user3 = address(0x3);
+    event Aux3IdRegistered(address indexed addr, uint256 id);
+    event Aux3IdTransferred(address indexed from, address indexed to, uint256 id);
+    event Aux3EventRegistered(address indexed contractAddress, uint256 indexed eventId, uint256 indexed aux3Id);
+    event Aux3EventUpdated(uint256 indexed eventId);
+    event Aux3IdDeleted(address indexed addr, uint256 id);
+    event Aux3EventDeleted(uint256 indexed eventId);
 
     function setUp() public {
-        registry = new Aux3Registry(msg.sender);
+        owner = address(this);
+        user1 = address(0x1);
+        user2 = address(0x2);
+        registry = new Aux3Registry(owner);
     }
 
     function testRegisterAux3Id() public {
-        uint256 id = registry.registerAux3Id(user1);
-        assertEq(id, 1);
-        assertEq(registry.getAux3Id(user1), 1);
-
-        // Try registering the same address again (should fail)
-        vm.expectRevert("Address is already registered");
-        registry.registerAux3Id(user1);
+        vm.prank(user1);
+        vm.expectEmit(true, true, false, true);
+        emit Aux3IdRegistered(user1, 1);
+        uint256 id = registry.registerAux3Id();
+        assertEq(id, 1, "First registered ID should be 1");
+        assertEq(registry.getAux3Id(user1), 1, "User1 should have ID 1");
     }
 
-    function testRegisterMultipleUsers() public {
-        uint256 id1 = registry.registerAux3Id(user1);
-        uint256 id2 = registry.registerAux3Id(user2);
-
-        assertEq(id1, 1);
-        assertEq(id2, 2);
-        assertEq(registry.getAux3Id(user1), 1);
-        assertEq(registry.getAux3Id(user2), 2);
+    function testCannotRegisterTwice() public {
+        vm.startPrank(user1);
+        registry.registerAux3Id();
+        vm.expectRevert("Address is already registered");
+        registry.registerAux3Id();
+        vm.stopPrank();
     }
 
     function testTransferAux3Id() public {
-        registry.registerAux3Id(user1);
-
-        // Transfer from user1 to user2
         vm.prank(user1);
+        uint256 id = registry.registerAux3Id();
+
+        vm.prank(user1);
+        vm.expectEmit(true, true, true, true);
+        emit Aux3IdTransferred(user1, user2, id);
         registry.transferAux3Id(user2);
 
-        assertEq(registry.getAux3Id(user1), 0);
-        assertEq(registry.getAux3Id(user2), 1);
+        assertEq(registry.getAux3Id(user1), 0, "User1 should no longer have an ID");
+        assertEq(registry.getAux3Id(user2), id, "User2 should now have the transferred ID");
     }
 
-    function testTransferAux3IdFailsForNonOwner() public {
-        registry.registerAux3Id(user1);
+    function testCannotTransferToSelf() public {
+        vm.prank(user1);
+        registry.registerAux3Id();
 
-        // Try to transfer from user2 (should fail)
-        vm.prank(user2);
-        vm.expectRevert("Sender does not have an ID");
-        registry.transferAux3Id(user3);
-    }
-
-    function testTransferAux3IdToSelf() public {
-        registry.registerAux3Id(user1);
-
-        // Try to transfer to self (should fail)
         vm.prank(user1);
         vm.expectRevert("Cannot transfer to the same address");
         registry.transferAux3Id(user1);
     }
 
-    function testTransferToInvalidAddress() public {
-        registry.registerAux3Id(user1);
-
-        // Try to transfer to the zero address (should fail)
+    function testCannotTransferWithoutId() public {
         vm.prank(user1);
-        vm.expectRevert("Invalid recipient address");
-        registry.transferAux3Id(address(0));
+        vm.expectRevert("Sender does not have an ID");
+        registry.transferAux3Id(user2);
     }
 
-    function testEmptyRegistryTransfer() public {
-        // Try to transfer when registry is empty (should fail)
+    function testDeleteAux3Id() public {
         vm.prank(user1);
-        vm.expectRevert("Registry is empty");
-        registry.transferAux3Id(user2);
+        uint256 id = registry.registerAux3Id();
+
+        vm.prank(user1);
+        vm.expectEmit(true, true, false, true);
+        emit Aux3IdDeleted(user1, id);
+        registry.deleteAux3Id();
+
+        assertEq(registry.getAux3Id(user1), 0, "User1 should no longer have an ID after deletion");
+    }
+
+    function testCannotDeleteNonexistentId() public {
+        vm.prank(user1);
+        vm.expectRevert("Sender does not own any Aux3Id");
+        registry.deleteAux3Id();
+    }
+
+    function testRegisterAux3Event() public {
+        vm.prank(user1);
+        registry.registerAux3Id();
+
+        vm.prank(user1);
+        vm.expectEmit(true, true, true, true);
+        emit Aux3EventRegistered(address(0x123), 1, 1);
+        registry.registerAux3Event(
+            1, // chainId
+            address(0x123), // contractAddress
+            bytes32(uint256(1)), // topic_0
+            bytes32(0), // topic_1
+            bytes32(0), // topic_2
+            bytes32(0), // topic_3
+            abi.encodePacked("action") // eventAction
+        );
+
+        (Aux3EventConfig memory config, bytes memory action) = registry.getAux3Event(1);
+        assertEq(config.chainId, 1, "ChainId should match");
+        assertEq(config.contractAddress, address(0x123), "Contract address should match");
+        assertEq(config.topic_0, bytes32(uint256(1)), "Topic 0 should match");
+        assertEq(action, abi.encodePacked("action"), "Event action should match");
+    }
+
+    function testUpdateAux3EventAction() public {
+        vm.prank(user1);
+        registry.registerAux3Id();
+
+        vm.prank(user1);
+        registry.registerAux3Event(
+            1, address(0x123), bytes32(uint256(1)), bytes32(0), bytes32(0), bytes32(0), abi.encodePacked("action")
+        );
+
+        vm.prank(user1);
+        vm.expectEmit(true, false, false, false);
+        emit Aux3EventUpdated(1);
+        registry.updateAux3EventAction(1, abi.encodePacked("new action"));
+
+        (, bytes memory newAction) = registry.getAux3Event(1);
+        assertEq(newAction, abi.encodePacked("new action"), "Event action should be updated");
+    }
+
+    function testDeleteAux3Event() public {
+        vm.prank(user1);
+        registry.registerAux3Id();
+
+        vm.prank(user1);
+        registry.registerAux3Event(
+            1, address(0x123), bytes32(uint256(1)), bytes32(0), bytes32(0), bytes32(0), abi.encodePacked("action")
+        );
+
+        vm.prank(user1);
+        vm.expectEmit(true, false, false, false);
+        emit Aux3EventDeleted(1);
+        registry.deleteAux3Event(1);
+
+        vm.expectRevert("Event does not exist");
+        registry.getAux3Event(1);
+    }
+
+    function testSweepNativeToken() public {
+        // Send some Ether to the contract
+        vm.deal(address(registry), 1 ether);
+
+        uint256 initialBalance = address(owner).balance;
+
+        vm.prank(owner);
+        registry.sweepNativeToken();
+
+        assertEq(address(registry).balance, 0, "Contract should have 0 balance after sweep");
+        assertEq(address(owner).balance, initialBalance + 1 ether, "Owner should receive the swept Ether");
+    }
+
+    // Add this function to allow the contract to receive Ether
+    receive() external payable {}
+
+    function testSweepERC20Token() public {
+        // Deploy a mock ERC20 token
+        MockERC20 token = new MockERC20();
+        token.mint(address(registry), 1000);
+
+        registry.sweepToken(IERC20(address(token)));
+
+        assertEq(token.balanceOf(address(registry)), 0, "Contract should have 0 token balance after sweep");
+        assertEq(token.balanceOf(owner), 1000, "Owner should receive the swept tokens");
+    }
+}
+
+// Mock ERC20 token for testing
+contract MockERC20 is IERC20 {
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint256)) public override allowance;
+    uint256 public override totalSupply;
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(allowance[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        return true;
+    }
+
+    function mint(address to, uint256 amount) public {
+        balanceOf[to] += amount;
+        totalSupply += amount;
     }
 }
